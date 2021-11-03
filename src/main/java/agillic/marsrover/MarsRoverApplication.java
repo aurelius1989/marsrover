cdd81b2cf6484f1ed2c0e8bb3b700c984dba5285
aurel.deac <aurel.deac@metro.digital>
2021-11-03 18:32:29 +0200

    initial commit

---
+++ src/main/java/agillic/marsrover/MarsRoverApplication.java
@@ -0,0 +1,14 @@
+package agillic.marsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApplication.class, args);
+
+		System.out.println("HELLO MARS ROVER!");
+	}
+}
